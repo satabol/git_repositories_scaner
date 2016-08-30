29db16a45796fa9a666ddf275f5988c2f0bba624
azakharov <alex-zaharow@yandex.ru>
2016-08-30 13:07:58 +0300

    - init

---
+++ App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace git_repositories_watcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static TaskbarIcon _notifyIcon = null;
+        public static TaskbarIcon NotifyIcon
+        {
+            get
+            {
+                return _notifyIcon;
+            }
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _notifyIcon = (TaskbarIcon)Application.Current.FindResource("NotifyIcon");
+            String settingsFilePath = getSettingsFilePath();
+
+            string settings_params = null;
+            if (File.Exists(settingsFilePath) == false)
+            {
+                settings_params = @"{ 
+    paths : []
+}";
+                File.WriteAllText(settingsFilePath, settings_params);
+            }
+            settings_params = File.ReadAllText(settingsFilePath);
+            Settings settings = JsonConvert.DeserializeObject<Settings>(settings_params);
+
+            Console.WriteLine("Paths.Count=" + settings.Paths.Count);
+        }
+
+        // Определить имя файла конфигурации:
+        public static String getSettingsFilePath()
+        {
+            String iniFilePath = null;
+            string exe_file = typeof(git_repositories_watcher.App).Assembly.Location; // http://stackoverflow.com/questions/4764680/how-to-get-the-location-of-the-dll-currently-executing
+            iniFilePath = System.IO.Path.GetDirectoryName(exe_file) + "\\" + System.IO.Path.GetFileNameWithoutExtension(exe_file) + ".json";
+            return iniFilePath;
+        }
+
+    }
+}
---
+++ NotifyIconViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace git_repositories_watcher
+{
+    class NotifyIconViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                //return new DelegateCommand { CommandAction = () => { Application.Current.Shutdown(); } };
+                return new DelegateCommand { CommandAction = () => { App.Current.Shutdown(0); } };
+            }
+        }
+    }
+    /// <summary>
+    /// Simplistic delegate command for the demo.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }    
+}
